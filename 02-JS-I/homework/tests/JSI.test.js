657191fabe466573fce018605501f5e6535a97d7
juankzaba <juankzaba34@gmail.com>
2020-10-13 21:53:09 -0500

    finish homework 2

--- 02-JS-I/homework/tests/JSI.test.js
+++ 02-JS-I/homework/tests/JSI.test.js
@@ -72,7 +72,7 @@ describe('devolverString(str)', function() {
   });
 });
 
-describe('suma(x, y)', function() {
+describe('sumar(x, y)', function() {
   it('Deberia devolver la suma de los dos argumentos', function() {
     expect(suma(5, 5)).toBe(10);
     expect(suma(-1, 5)).toBe(4);
